16f8a0e31c331fdff39ba2a7ddaa667ee90d55f4
Farmayil001 <gasanoff8085@gmail.com>
2025-11-12 15:31:25 +0400

    fix deploy err

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,20 +1,23 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prismadb";
 
-interface Params {
-    params: { id: string };
-}
+export async function GET(
+    req: Request,
+    context: { params: { id: string } }
+) {
+    const { id } = context.params;
 
-export async function GET(req: Request, { params }: Params) {
     const product = await prisma.product.findUnique({
-        where: { id: params.id },
+        where: { id },
         include: {
             category: true,
-            reviews: { include: { user: true } } // review-ləri və review yazan useri gətir
-        }
+            reviews: { include: { user: true } },
+        },
     });
 
-    if (!product) return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    if (!product) {
+        return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    }
 
     return NextResponse.json(product);
 }
