91f63572101770e43f7b0781d5f09367049a6d38
Farmayil001 <gasanoff8085@gmail.com>
2025-11-24 15:06:19 +0400

    f

--- app/api/wishlist/route.ts
+++ app/api/wishlist/route.ts
@@ -4,10 +4,10 @@ import { getCurrentUser } from '@/app/actions/getCurrentUser'
 
 export async function GET() {
     const user = await getCurrentUser()
-    if (!user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    // if (!user) return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
 
     const wishlist = await prisma.wishlistItem.findMany({
-        where: { wishlist: { userId: user.id } },
+        where: { wishlist: { userId: user?.id } },
         include: { wishlist: true } // məhsul məlumatını da gətir
     })
 
